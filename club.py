1af3ec6407100b15ca650dbf40d31e796f0488dd
Adam Strike <astrike30@users.noreply.github.com>
2020-02-01 00:46:34 -0500

    Added Favorites field to club object

--- club.py
+++ club.py
@@ -1,6 +1,7 @@
 class Club:
 
-	def __init__(self, name, categories, description):
+	def __init__(self, name, categories, description, favourites=0):
 		self.name = name
 		self.categories = categories
 		self.description = description
+		self.favourites = 0
